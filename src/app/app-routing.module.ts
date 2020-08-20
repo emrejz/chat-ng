4b6beba78cf94331f4c91732e21fc537b6517aa1
emre kayacan <emre.kayacan@msn.com>
2020-08-20 23:35:12 +0300

    fixed refresh page bug

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,10 +6,10 @@ import { Routes, RouterModule } from "@angular/router";
 const routes: Routes = [
   { path: "sign", component: LoginComponent },
   { path: "chat", component: ChatComponent },
-  { path: "**", redirectTo: "chat", pathMatch: "full" }
+  { path: "**", redirectTo: "chat", pathMatch: "full" },
 ];
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
